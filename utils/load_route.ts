f27fc3224db6ec8fc3ba06f1a0af60ad8b85d32f
kwaitsing <131563118+kwaitsing@users.noreply.github.com>
2024-07-13 00:14:28 +0800

    Fixed some weird s**t

--- utils/load_route.ts
+++ utils/load_route.ts
@@ -1,8 +1,8 @@
-import Elysia, { t } from "elysia";
+import Elysia, { t, type HTTPMethod } from "elysia";
 import type { Result, RuntimeRoute, Request } from "../type";
 import { logger } from "toolbx";
 
-export const load_route = (routes: RuntimeRoute[], app: Elysia, gateway: ((...args: any[]) => Promise<Result>) |  ((...args: any[]) => any) , debug: boolean = false) => {
+export const load_route = (routes: RuntimeRoute[], app: Elysia, gateway: ((...args: any[]) => Promise<Result>) | ((...args: any[]) => any), debug: boolean = false) => {
   try {
     routes.forEach((obj) => {
       const index = app.routes.findIndex(route => route.path === obj.path);
@@ -17,64 +17,34 @@ export const load_route = (routes: RuntimeRoute[], app: Elysia, gateway: ((...ar
         }
       }
       if (index == -1) {
-        switch (obj.method) {
-          case 'get':
-            app.get(obj.path, async (contents) => {
-              try {
-                // log this api access to the console
-                const date = Math.floor(new Date().getTime() / 1000);
-                if (debug) logger(`audit> Unix Timestamp:${date} => ${JSON.stringify(contents)}`, 4);
-                // load route depends on its details
-                if (obj.isDirect) {
-                  return await obj.handler(contents)
-                } else {
-                  throw new Error("Method GET is restricted to direct route only");
-                }
-              } catch (error) {
-                let errMsg = String(error);
-                if (error instanceof Error) {
-                  errMsg = error.message;
-                }
-                const result: Result = {
-                  status: 'er',
-                  data: {
-                    msg: errMsg
-                  }
-                }
-                return JSON.stringify(result)
+        const allowMethods = ['get', 'delete', 'post']
+        if (!allowMethods.includes(obj.method)) throw new Error("Unknown methods" + obj.method);
+        // @ts-ignore
+        app[obj.method](obj.path, async (contents) => {
+          try {
+            // log this api access to the console
+            const date = Math.floor(new Date().getTime() / 1000);
+            if (debug) logger(`audit> Unix Timestamp:${date} => ${JSON.stringify(contents)}`, 4);
+            // load route depends on its details
+            if (obj.isDirect) {
+              return await obj.handler(contents)
+            } else {
+              return await gateway(contents, (contents: any) => obj.handler(contents))
+            }
+          } catch (error) {
+            let errMsg = String(error);
+            if (error instanceof Error) {
+              errMsg = error.message;
+            }
+            const result: Result = {
+              status: 'er',
+              data: {
+                msg: errMsg
               }
-            }, obj.addon)
-            break;
-          case 'post':
-            app.post(obj.path, async (contents) => {
-              try {
-                // log this api access to the console
-                const date = Math.floor(new Date().getTime() / 1000);
-                if (debug) logger(`audit> Unix Timestamp:${date} => ${JSON.stringify(contents)}`, 4);
-                // load route depends on its details
-                if (obj.isDirect) {
-                  return await obj.handler(contents)
-                } else {
-                  return await gateway(contents, (contents: any) => obj.handler(contents))
-                }
-              } catch (error) {
-                let errMsg = String(error);
-                if (error instanceof Error) {
-                  errMsg = error.message;
-                }
-                const result: Result = {
-                  status: 'er',
-                  data: {
-                    msg: errMsg
-                  }
-                }
-                return JSON.stringify(result)
-              }
-            }, obj.addon)
-            break;
-          default:
-            throw new Error(`Unknown METHOD => ${obj.method}`);
-        }
+            }
+            return JSON.stringify(result)
+          }
+        }, obj.addon)
       } else {
         logger(`[Route Loader] > Skip loading ${obj.path}, conflict detected`, 4)
       }
