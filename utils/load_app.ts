1d9b02635a1c521bf5582afad5a944304b2d5a3e
kwaitsing <131563118+kwaitsing@users.noreply.github.com>
2024-07-19 11:36:13 +0800

    @load_app try catch fixed

--- utils/load_app.ts
+++ utils/load_app.ts
@@ -3,28 +3,38 @@ import { readdir } from "node:fs/promises";
 import Elysia from "elysia";
 
 export const load_app = async (app: Elysia, debug: boolean = false) => {
-    // Load extensions first
-    const externalModules = await readdir(`${import.meta.dir}/../../../src/app/external`)
-    for (const module of externalModules) {
-        const loader = await import(`${import.meta.dir}/../../../src/app/external/${module}/loader.ts`)
-        if (typeof loader.loader === 'function') {
-            if (debug) logger(`[APP Loader] > Loading (external) ${module}`, 4)
-            await loader.loader(app)
-        } else {
-            logger(`[APP Loader] > (external) ${module} is corrupted with type of ${typeof loader}`, 2)
-            process.exit(1)
+    try {
+        // Load extensions first
+        const externalModules = await readdir(`${import.meta.dir}/../../../src/app/external`)
+        for (const module of externalModules) {
+            try {
+                const loader = await import(`${import.meta.dir}/../../../src/app/external/${module}/loader.ts`)
+                if (typeof loader.loader === 'function') {
+                    if (debug) logger(`[APP Loader] > Loading (external) ${module}`, 4)
+                    await loader.loader(app)
+                } else {
+                    throw new Error(`(external) ${module} is corrupted with type of ${typeof loader}`);
+                }
+            } catch (error) {
+                logger(`[APP Loader @app/${module}] > ${error}`, 2)
+            }
         }
-    }
-    // Load internal then
-    const internalModules = await readdir(`${import.meta.dir}/../../../src/app/internal`)
-    for (const module of internalModules) {
-        const loader = await import(`${import.meta.dir}/../../../src/app/internal/${module}/loader.ts`)
-        if (typeof loader.loader === 'function') {
-            if (debug) logger(`[APP Loader] > Loading (internal) ${module}`, 4)
-            await loader.loader(app)
-        } else {
-            logger(`[APP Loader] > (internal) ${module} is corrupted with type of ${typeof loader}`, 2)
-            process.exit(1)
+        // Load internal then
+        const internalModules = await readdir(`${import.meta.dir}/../../../src/app/internal`)
+        for (const module of internalModules) {
+            try {
+                const loader = await import(`${import.meta.dir}/../../../src/app/internal/${module}/loader.ts`)
+                if (typeof loader.loader === 'function') {
+                    if (debug) logger(`[APP Loader] > Loading (internal) ${module}`, 4)
+                    await loader.loader(app)
+                } else {
+                    throw new Error(`(internal) ${module} is corrupted with type of ${typeof loader}`)
+                }
+            } catch (error) {
+                logger(`[APP Loader @app/${module}] > ${error}`, 2)
+            }
         }
+    } catch (error) {
+        logger(`[APP Loader] > ${error}`, 2)
     }
 }
